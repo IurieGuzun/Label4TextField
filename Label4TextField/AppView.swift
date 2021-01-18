064dfd8dbb027a53a90e72bd5f1bf6255022718f
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-18 11:05:03 -0500

    Before plist.info update

---
+++ Label4TextField/AppView.swift
@@ -0,0 +1,23 @@
+//
+//  AppView.swift
+//  Label4TextField
+//
+//  Created by Iurie Guzun on 2021-01-18.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    @available(iOS 13.0.0, *)
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    @available(iOS 13.0.0, *)
+    static var previews: some View {
+        AppView()
+    }
+}
