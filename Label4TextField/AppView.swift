18e5b4266fac6434d66d5658cb9b9d98a557b800
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-18 13:51:34 -0500

    Accessed SwiftUI AppView

--- Label4TextField/AppView.swift
+++ Label4TextField/AppView.swift
@@ -9,14 +9,18 @@
 import SwiftUI
 
 struct AppView: View {
-    @available(iOS 13.0.0, *)
+//    @available(iOS 13.0.0, *)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        print("AppView Accessed!")
+        return Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .fontWeight(.heavy)
+            .foregroundColor(.green)
+            .background(Color.blue)
     }
 }
 
 struct AppView_Previews: PreviewProvider {
-    @available(iOS 13.0.0, *)
+//    @available(iOS 13.0.0, *)
     static var previews: some View {
         AppView()
     }
--- Label4TextField/SceneDelegate.swift
+++ Label4TextField/SceneDelegate.swift
@@ -18,12 +18,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Use a UIHostingController as window root view controller
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: AppView())  // Modified By Iurie
-        self.window = window
-        window.makeKeyAndVisible()
+        // Create the SwiftUI view that provides the window contents.
+     //   let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: AppView())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
+
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+//
+//        // Use a UIHostingController as window root view controller
+//        let window = UIWindow(frame: UIScreen.main.bounds)
+//        window.rootViewController = UIHostingController(rootView: AppView())  // Modified By Iurie
+//        print("SceneDelegate Accessed!")
+//        self.window = window
+//        window.makeKeyAndVisible()
+//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
